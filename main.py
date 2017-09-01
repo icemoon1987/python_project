c830db7ef6af369e9ae6250851668e1be874bf09
icemoon1987 <panwenhai1987@163.com>
2017-09-01 16:34:21 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+######################################################
+#
+# File Name:  main.py
+#
+# Function:   
+#
+# Usage:  
+#
+# Input:  
+#
+# Output:	
+#
+# Author: wenhai.pan
+#
+# Create Time:    2017-09-01 15:45:04
+#
+######################################################
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import os
+import time
+import json
+import shutil
+import logging
+from datetime import datetime, timedelta
+
+
+def init(conf_file):
+    """
+    Initilization process
+    
+    args:
+        conf_file   configration file
+
+    return: none
+    """
+
+    with open(conf_file, "r") as f:
+        conf_obj = json.loads(f.read())
+
+    # init directories
+    log_dir = conf_obj["log_dir"]
+    data_dir = conf_obj["data_dir"]
+    result_dir = conf_obj["result_dir"]
+
+    if conf_obj["clear_data_every_time"] and os.path.exists(data_dir):
+        shutil.rmtree(data_dir)
+
+    if conf_obj["clear_result_every_time"] and os.path.exists(result_dir):
+        shutil.rmtree(result_dir)
+
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
+
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+
+    # init logging
+    logging.basicConfig(level="INFO", filename="%s/%s.%s" % (log_dir, __file__, datetime.now().strftime("%Y%m%d")), filemode='a', format='%(asctime)s [%(levelname)s] [%(filename)s] [%(funcName)s] [%(lineno)d] %(message)s')
+
+    return conf_obj
+
+
+def print_help():
+    print "USAGE: python %s CONF_FILE" % (__file__)
+    return
+
+
+def main():
+
+    conf_file = "./conf/config.json"
+
+    if len(sys.argv) >= 2:
+        conf_file = sys.argv[1]
+
+    # Init
+    conf_obj = init(conf_file)
+    logging.info("init finish")
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
